7d743069364d8d4db6cf0ad2d8abcfcc12d7e4ef
c100bit <c100bit@gmail.com>
2023-06-20 17:25:29 +0300

    fix

---
+++ cmd/producer/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"eventdrivenrabbit/internal"
+	"log"
+	"time"
+
+	"github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	conn, err := internal.ConnectRabbitMQ("percy", "secret", "localhost:5672", "customers")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	client, err := internal.NewRabbitMQClient(conn)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	if err := client.CreateQueue("customers_created", true, false); err != nil {
+		panic(err)
+	}
+
+	if err := client.CreateQueue("customers_test", false, true); err != nil {
+		panic(err)
+	}
+
+	if err := client.CreateBinding("customers_created", "customers.created.*", "customer_events"); err != nil {
+		panic(err)
+	}
+
+	if err := client.CreateBinding("customers_test", "customers.*", "customer_events"); err != nil {
+		panic(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := client.Send(ctx, "customer_events", "customers.created.us", amqp091.Publishing{
+		ContentType:  "text/plain",
+		DeliveryMode: amqp091.Persistent,
+		Body:         []byte(`An cool message between services`),
+	}); err != nil {
+		panic(err)
+	}
+
+	if err := client.Send(ctx, "customer_events", "customers.test", amqp091.Publishing{
+		ContentType:  "text/plain",
+		DeliveryMode: amqp091.Transient,
+		Body:         []byte(`An uncool message between services`),
+	}); err != nil {
+		panic(err)
+	}
+
+	time.Sleep(time.Second * 5)
+	log.Println(client)
+
+}
---
+++ internal/rabbitmq.go
@@ -0,0 +1,53 @@
+package internal
+
+import (
+	"context"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type RabbitClient struct {
+	conn *amqp.Connection
+	ch   *amqp.Channel
+}
+
+func ConnectRabbitMQ(username, password, host, vhost string) (*amqp.Connection, error) {
+	return amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s/%s", username, password, host, vhost))
+}
+
+func NewRabbitMQClient(conn *amqp.Connection) (RabbitClient, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return RabbitClient{}, err
+	}
+
+	return RabbitClient{
+		conn: conn,
+		ch:   ch,
+	}, nil
+}
+
+func (rn RabbitClient) Close() error {
+	return rn.ch.Close()
+}
+
+func (rc RabbitClient) CreateQueue(queueName string, durable, autoDelete bool) error {
+	_, err := rc.ch.QueueDeclare(queueName, durable, autoDelete, false, false, nil)
+	return err
+}
+
+func (rc RabbitClient) CreateBinding(name, binding, exchange string) error {
+	return rc.ch.QueueBind(name, binding, exchange, false, nil)
+}
+
+func (rc RabbitClient) Send(ctx context.Context, exchange, routingKey string, options amqp.Publishing) error {
+	return rc.ch.PublishWithContext(
+		ctx,
+		exchange,
+		routingKey,
+		true,
+		false,
+		options,
+	)
+}
